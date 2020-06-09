43784734579a61b649ea3cb6b88cd78518d7798d
Brian Mangus <b.mangus@me.com>
2020-06-08 22:34:31 -0500

    updated bt workflow service to use config helper

--- app/Services/BTWorkflowService.php
+++ app/Services/BTWorkflowService.php
@@ -9,10 +9,10 @@ class BTWorkflowService extends FluentFMRepository
     public function __construct()
     {
         parent::__construct([
-            'file' => env('BT_FM_FILE', 'SomeFileSomewhere'),
-            'host' => env('BT_FM_HOST', '127.1.2.1'),
-            'user' => env('BT_FM_USERNAME', 'user'),
-            'pass' => env('BT_FM_PASSWORD', 'password'),
+            'file' => config('app.bt_file'),
+            'host' => config('app.bt_host'),
+            'user' => config('app.bt_username'),
+            'pass' => config('app.bt_password'),
         ]);
     }
 }
