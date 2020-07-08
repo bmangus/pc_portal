15be056ad6ffac5f3b421377ff88aa25c4f22c8e
Brian Mangus <b.mangus@me.com>
2020-07-08 10:16:43 -0500

    Updated migration to allow large numbers in AT.

---
+++ database/migrations/2020_07_08_095017_update_at_requisition_items_floats.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateAtRequisitionItemsFloats extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(Schema::hasTable('a_t_requisition_items')){
+            Schema::table('a_t_requisition_items', function(Blueprint $table){
+                $table->float('Total', 20, 2)->change();
+                $table->float('unitPrice', 20, 2)->change();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
