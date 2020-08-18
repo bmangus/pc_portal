2dc496239650fb21e3453fcf80697f3f7be5b1d5
Brian Mangus <b.mangus@me.com>
2020-08-18 07:48:56 -0500

    Added migration to rename approved status 3 column to correct case.

---
+++ database/migrations/2020_08_18_073621_modify_approver_3_status_column_name.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyApprover3StatusColumnName extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(Schema::hasTable('b_t_requisitions')){
+            Schema::table('b_t_requisitions', function(Blueprint $table){
+                $table->renameColumn('Approvedstatus3', 'ApprovedStatus3');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
