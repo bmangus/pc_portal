e8982c4457707378a94747c3c3396aefc09aa48d
Brian Mangus <bmangus@users.noreply.github.com>
2021-02-08 20:49:59 -0600

    Revert "Shift 32942"

--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'ldap',
-            'model' => App\Models\User::class,
+            'model' => App\User::class,
         ],
 
         // 'users' => [
